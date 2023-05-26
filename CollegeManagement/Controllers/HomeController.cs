1998e6cc61d8621b61722b1dc070839840d7a1d4
SAURABH_YADAV <SAURABH_YADAV@DESKTOP-60624KN>
2023-05-26 17:03:49 +0530

    Add project files.

---
+++ CollegeManagement/Controllers/HomeController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CollegeManagement.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult ACADEMICS()
+        {
+            return View();
+        }
+        public ActionResult Admission()
+        {
+            return View();
+        }
+    }
+}
